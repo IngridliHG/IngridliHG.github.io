83f8f44c080a548cebcda9c567f05dcab7cdf51b
Ingrid <147679098+IngridliHG@users.noreply.github.com>
2023-10-27 13:11:17 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #eb94ff;'>|</span>",
+ cursor: "<span style='color: #fb4993;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: ##00baff;">Ellos insisten en tener prejuicios, pero aun así sigo mi camino, no sé lo que es rendirse.</span>')
+  .typeString('<span style="color: #99662d">Ellos insisten en tener prejuicios, pero aun así sigo mi camino, no sé lo que es rendirse.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
